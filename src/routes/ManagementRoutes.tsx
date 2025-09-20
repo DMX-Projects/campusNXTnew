f4eefbf033d6e186d87d85bffb897d0825e993c4
Nitheesh-Chinnusamy <nitheeshchinnusamy21@gmail.com>
2025-09-20 15:45:45 +0530

    nav changes for pricipal Administration

--- src/routes/ManagementRoutes.tsx
+++ src/routes/ManagementRoutes.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+//Admission Management Components
+import AdmissionDashboard from "../pages/RegistorModule/Admission/RegistrarDashboard";
+import CAPAdmin from "../pages/RegistorModule/Admission/AdminssionProcess/CAPAdmission/CAPAdmin";
+import SeatAllotmentPhaseI from "../pages/RegistorModule/Admission/AdminssionProcess/CAPAdmission/SeatAllotmentPhaseI";
+import SeatAllotmentPhaseII from "../pages/RegistorModule/Admission/AdminssionProcess/CAPAdmission/SeatAllotmentPhaseII";
+import SeatAllotmentPhaseIII from "../pages/RegistorModule/Admission/AdminssionProcess/CAPAdmission/SeatAllotmentPhaseIII";
+import ManagementAdmissionProcess from "../pages/RegistorModule/Admission/AdminssionProcess/ManagementAdmission/ManagementAdmissionProcess";
+import SpotAdmissionMerit from "../pages/RegistorModule/Admission/AdminssionProcess/SpotAdmission/SpotAdmissionMerit";
+import SpotAdmissionFCFS from "../pages/RegistorModule/Admission/AdminssionProcess/SpotAdmission/SpotAdmissionFCFS";
 const CommonRoutes: React.FC = () => {
   return (
     <Routes>
+      <Route path="/Admission/dashboard" element={<AdmissionDashboard />} />
+      <Route path="/admission-process/cap" element={<CAPAdmin />} />
+      <Route path="/admission-process/seat-allotment/phase-1" element={<SeatAllotmentPhaseI />} />
+      <Route path="/admission-process/seat-allotment/phase-2" element={<SeatAllotmentPhaseII />} />
+      <Route path="/admission-process/seat-allotment/phase-3" element={<SeatAllotmentPhaseIII />} />
+      <Route path="/admission-process/management-quota" element={<ManagementAdmissionProcess />} />
+      <Route path="/admission-process/spot-admission/merit-based" element={<SpotAdmissionMerit />} />
+      <Route path="/admission-process/spot-admission/first-come" element={<SpotAdmissionFCFS />} />
     </Routes>
   );
 };
